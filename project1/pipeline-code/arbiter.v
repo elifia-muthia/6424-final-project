e0cd01066a162d4d624d55fa3c9f89afb64f2c1d
Elifia Muthia <elifiamuthia@Elifias-MacBook-Air.local>
2025-04-22 15:38:11 -0400

    started on arbiter

--- project1/pipeline-code/arbiter.v
+++ project1/pipeline-code/arbiter.v
@@ -6,5 +6,39 @@ module arbiter (
     output wire grant_1,
     output wire grant_2
 );
-// Arbitration logic placeholder
+
+    reg last_grant;  // 0 (last gave to 1) or 1 (last gave to 2)
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            grant_1   <= 0;
+            grant_2   <= 0;
+            last_grant<= 0;
+        end else begin
+            if (req_1 && req_2) begin
+                // take turns
+                if (last_grant) begin
+                    grant_1    <= 1;
+                    grant_2    <= 0;
+                    last_grant <= 0;
+                end else begin
+                    grant_1    <= 0;
+                    grant_2    <= 1;
+                    last_grant <= 1;
+                end
+            end else if (req_1) begin
+                grant_1    <= 1;
+                grant_2    <= 0;
+                last_grant <= 0;
+            end else if (req_2) begin
+                grant_1    <= 0;
+                grant_2    <= 1;
+                last_grant <= 1;
+            end else begin
+                grant_1 <= 0;
+                grant_2 <= 0;
+            end
+        end
+    end
+
 endmodule
