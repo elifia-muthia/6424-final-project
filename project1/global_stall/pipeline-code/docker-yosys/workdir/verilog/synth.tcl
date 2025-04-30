1202c516137dfdf96ed3b7d2822ac5b8ba7f8c89
ShinyMiraidon <bind-naps0r@icloud.com>
2025-04-30 10:55:46 -0400

    Chore: Moved verilog files into their own folder

--- project1/global_stall/pipeline-code/docker-yosys/workdir/verilog/synth.tcl
+++ project1/global_stall/pipeline-code/docker-yosys/workdir/verilog/synth.tcl
@@ -13,16 +13,16 @@ read_verilog $verilog_files
 prep -top $top_module
 
 # Read all Liberty files for different process corners
-read_liberty -ignore_miss_func ./libs/asap7-std-cells-lib-merged.lib
+read_liberty -ignore_miss_func ../libs/asap7-std-cells-lib-merged.lib
 
 # Perform generic synthesis
 synth -top $top_module
 
 # Map the design to the flip flop cells in Liberty files
-dfflibmap -liberty ./libs/asap7-std-cells-lib-merged.lib
+dfflibmap -liberty ../libs/asap7-std-cells-lib-merged.lib
 
 # Map the design to the standard cells in Liberty files
-abc -liberty ./libs/asap7-std-cells-lib-merged.lib
+abc -liberty ../libs/asap7-std-cells-lib-merged.lib
 
 # Perform technology mapping
 opt_clean -purge
