21384e8f0c068814be6bc0a499ce62a98ce8fa89
ShinyMiraidon <bind-naps0r@icloud.com>
2025-05-03 13:41:21 -0400

    Fix: reversed direction of flush propogation

--- project1/global_stall/pipeline-code/docker-yosys/workdir/verilog/pipeline_unit.v
+++ project1/global_stall/pipeline-code/docker-yosys/workdir/verilog/pipeline_unit.v
@@ -12,7 +12,7 @@ module pipeline_unit (
 // actual pipeline stages here
 reg [31:0] stage_1_output, stage_2_output, stage_3_output;
 reg stage_1_valid, stage_2_valid, stage_3_valid;
-reg stage_1_flush, stage_2_flush;
+reg stage_2_flush, stage_3_flush;
 
 assign outputs  = stage_3_output;
 assign out_valid = stage_3_valid;
@@ -28,11 +28,11 @@ always @(posedge clk or posedge reset) begin
         stage_1_flush <= 0;
         stage_2_flush <= 0;
     end else begin
-        stage_1_flush <= stage_2_flush;
-        stage_2_flush <= flush;
+        stage_2_flush <= stage_1_flush;
+        stage_3_flush <= stage_2_flush;
 
         // stage 1 
-        if (stage_1_flush) begin
+        if (flush) begin
             stage_1_valid <= 0;
             stage_1_output <= 0;
         end else if (stall) begin
@@ -56,7 +56,7 @@ always @(posedge clk or posedge reset) begin
         end
 
         // stage 3 
-        if (flush) begin
+        if (stage_3_flush) begin
             stage_3_valid <= 0;
             stage_3_output <= 0;
         end else if (stall) begin
