569f5a65842a173f153ebd38ee40687a2d3d173c
ShinyMiraidon <bind-naps0r@icloud.com>
2025-05-06 12:46:44 -0400

    Feat: Made producer latency insensitive

--- project1/pipelined_stall/pipeline-code/docker-yosys/workdir/verilog/producer_fsm.v
+++ project1/pipelined_stall/pipeline-code/docker-yosys/workdir/verilog/producer_fsm.v
@@ -16,6 +16,8 @@ module producer_fsm (
 
 reg flush_1, flush_2, valid_1, valid_2;
 
+wire fire_1, fire_2;
+
 assign out_valid_1 = valid_1;
 assign out_valid_2 = valid_2;
 assign out_flush_1 = flush_1;
@@ -26,6 +28,9 @@ reg [31:0] counter_1, counter_2;
 assign pipeline1_inputs = counter_1;
 assign pipeline2_inputs = counter_2;
 
+assign fire_1 = !(in_stall_1 & valid_1);
+assign fire_2 = !(in_stall_2 & valid_2);
+
 always @(posedge clk or posedge reset) begin
     if(reset) begin
         valid_1 <= 0;
@@ -35,27 +40,26 @@ always @(posedge clk or posedge reset) begin
         counter_1 <= 0;
         counter_2 <= 1;
     end else begin
-        if (in_stall_1) begin
+
+        if(counter_1[7:0] == 0) begin
+            flush_1 <= 1;
             valid_1 <= 0;
-            counter_1 <= counter_1;
-        end else begin
-            counter_1 <= counter_1 + 2;
-            valid_1 <= 1;
+        end
+        else begin 
+            flush_1 <= 0;
+            valid_1 <= (in_stall_1 & valid_1) ? 1 : fire_1;
+            if (fire_1) counter_1 <= counter_1 + 1;
         end
 
-        if (in_stall_2) begin
+        if(counter_2[7:0] == 1) begin
+            flush_2 <= 1;
             valid_2 <= 0;
-            counter_2 <= counter_2;
-        end else begin
-            counter_2 <= counter_2 + 2;
-            valid_2 <= 1;
         end
-
-        if(counter_1[7:0] == 0) flush_1 <= 1;
-        else flush_1 <= 0;
-
-        if(counter_2[7:0] == 1) flush_2 <= 1;
-        else flush_2 <= 0;
+        else begin 
+            flush_2 <= 0;
+            valid_2 <= (in_stall_2 & valid_2) ? 1 : fire_2;
+            if (fire_2) counter_2 <= counter_2 + 1;
+        end
     end
 end
 
